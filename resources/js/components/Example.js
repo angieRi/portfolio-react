da9f18079c45f63afc6d505ccb62eb51b72353b5
angela <ri.angela07@gmail.com>
2022-04-13 23:16:07 -0300

    installation

---
+++ resources/js/components/Example.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function Example(){
+    return <p>Hola React</p>
+}
+
+export default Example;
+ReactDom.render(<Example />, document.getElementById("example"));
