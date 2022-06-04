276db98650032d7f1d73ff1ac5ea44efb92750f3
angela <ri.angela07@gmail.com>
2022-06-04 01:54:15 -0300

    update css

--- resources/js/components/header/CVD.jsx
+++ resources/js/components/header/CVD.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CVD = () => {
     return (
         <div className='cvd'>
-            <a href="http://127.0.0.1:8000/api/download" download className='btn'>Descargar CV</a>
+            <a href="https://angeladeveloper.herokuapp.com/api/download" download className='btn'>Descargar CV</a>
             <a href="#contact"  className='btn btn-primary'>Contáctame</a>
         </div>
     );
